759fd8dc48b3fbc3eb408084c55fe85c139910a4
CookedIdeas <113336281+CookedIdeas@users.noreply.github.com>
2022-12-14 17:58:35 +0100

    Step 1.2 : filter works by category

--- FrontEnd/js/fetch-works.js
+++ FrontEnd/js/fetch-works.js
@@ -1,13 +1,17 @@
 import { API_URL } from "./config.js";
 
+// ================== DISPLAY FUNCTION ================== //
+
 //access the div gallery where works will be displayed
 const galleryElement = document.querySelector(".gallery");
 
 const displayWorks = (fetchedWorks) => {
-  console.log(fetchedWorks);
   for (let work of fetchedWorks) {
     //create a figure for each work
     let figureElement = document.createElement("figure");
+
+    //give data-category with the right category
+    figureElement.setAttribute("data-category", work.categoryId);
 
     //create a img with src and alt attributes for each work
     let imgElement = document.createElement("img");
@@ -29,8 +33,49 @@ const displayWorks = (fetchedWorks) => {
   }
 };
 
+// ================== FILTER FUNCTION ================== //
+
+const filterButtons = document.getElementsByTagName("button");
+
+export const filterFunction = async () => {
+  for (let filter of filterButtons) {
+    filter.addEventListener("click", function () {
+      //if there is a previously selected filterButton, remove his selectedInput class
+      const prevSelectedFilter =
+        document.getElementsByClassName("selectedInput");
+      if (prevSelectedFilter.length > 0) {
+        prevSelectedFilter[0].classList.remove("selectedInput");
+      }
+
+      this.classList.add("selectedInput");
+      const localStoredWorks = JSON.parse(window.localStorage.getItem("works"));
+
+      //get the categoryId of the clicked filterButton
+      const selectedCategory = Number(this.value);
+
+      //if categoryId > 0, clicked filterButton != all
+      if (selectedCategory > 0) {
+        //filter localy stored works by their categoryId
+        const filteredWorks = localStoredWorks.filter(function (work) {
+          return work.categoryId === selectedCategory;
+        });
+
+        //empty the gallery div
+        galleryElement.replaceChildren();
+
+        //display the filter function result
+        displayWorks(filteredWorks);
+      } else {
+        //display all is selected so empty gallery div and display all localy stored works
+        galleryElement.replaceChildren();
+        displayWorks(localStoredWorks);
+      }
+    });
+  }
+};
+
 //get works and if response is ok, display works in page
-const fetchWorks = fetch(API_URL + "works", {
+const fetchWorks = await fetch(API_URL + "works", {
   method: "GET",
 })
   .then(function (res) {
@@ -39,7 +84,9 @@ const fetchWorks = fetch(API_URL + "works", {
     }
   })
   .then(function (works) {
+    window.localStorage.setItem("works", JSON.stringify(works));
     displayWorks(works);
+    filterFunction();
   })
   .catch(function (error) {
     console.log(error);
---
+++ FrontEnd/js/works-filters.js
@@ -0,0 +1,5 @@
+const category = [
+  { id: 1, name: "Objets" },
+  { id: 2, name: "Appartements" },
+  { id: 3, name: "Hotels & restaurants" },
+];
