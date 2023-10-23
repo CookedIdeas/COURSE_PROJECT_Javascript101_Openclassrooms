09284f216d66b7fb6fb60e89053d831b7e18cfd0
CookedIdeas <113336281+CookedIdeas@users.noreply.github.com>
2023-10-23 15:18:22 +0200

    Adding readme

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,16 +1,16 @@
-import { enableAdminMode } from "./admin-mode.js";
-import { getCookie } from "./cookie-management.js";
-import { FetchAPIgetWorks } from "./fetch-works.js";
-import { logUser } from "./login-user.js";
+import { enableAdminMode } from './admin-mode.js';
+import { getCookie } from './cookie-management.js';
+import { FetchAPIgetWorks } from './fetch-works.js';
+import { logUser } from './login-user.js';
 
 const pageUrl = window.location.pathname;
-const thisPageName = pageUrl.split("/")[pageUrl.split("/").length - 1];
+const thisPageName = pageUrl.split('/')[pageUrl.split('/').length - 1];
 
-if (thisPageName === "index.html") {
+if (thisPageName === 'index.html') {
   FetchAPIgetWorks();
-  const userToken = getCookie("token");
+  const userToken = getCookie('token');
   enableAdminMode(userToken);
-} else if (thisPageName === "login.html") {
-  const loginButton = document.getElementById("loginButton");
-  loginButton.addEventListener("click", logUser);
+} else if (thisPageName === 'login.html') {
+  const loginButton = document.getElementById('loginButton');
+  loginButton.addEventListener('click', logUser);
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+# A course project with Openclassrooms : begin with javascript
+
+Second project of web development training.
+
+## Key learning
+
+- Make a website dynamic with javascript
+- Fetch data with async javascript
+- Debug javascript with DevTools
+
+![Screenshot](githubAssets/screenshot.png)
